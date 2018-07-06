bcb8d2cd3b3963eb61062501878480b184dc6a7b
Tomek <K4masz@gmail.com>
2018-07-06 08:55:37 +0200

    init

---
+++ src/main/java/eu/tpawlik/SpringVaadinTutorial/SpringVaadinTutorialApplication.java
@@ -0,0 +1,12 @@
+package eu.tpawlik.SpringVaadinTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVaadinTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVaadinTutorialApplication.class, args);
+	}
+}
